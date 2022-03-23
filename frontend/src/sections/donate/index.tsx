b81f479dc972060f9d68a3e397dc5415710e576d
Victor Jimenez <vic.jicama@gmail.com>
2022-03-23 09:37:55 -0600

    Add donate massages and section

---
+++ frontend/src/sections/donate/index.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Container, Row, Col, Button } from "reactstrap";
+import { RESTAURANT_FIND } from "common/queries/restaurant";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { FormattedMessage } from "react-intl";
+
+export const Component = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <p>
+            <FormattedMessage id="msg.intro" />{" "}
+            <b>
+              <a href="https://wck.org/" target="_blank">
+                World Central Kitchen
+              </a>
+            </b>
+          </p>
+          <p>
+            <FormattedMessage id="msg.goal" />
+          </p>
+          <p>
+            <FormattedMessage id="msg.cta" />
+          </p>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-center">
+          <Button
+            className="text-capitalize"
+            color="success"
+            onClick={() => {
+              window.open(
+                `https://donate.wck.org/give/f3789323/#!/donation/checkout`,
+                "_blank"
+              );
+            }}
+          >
+            <FormattedMessage id="app.donate" />
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+/*
+const {
+  viewer: {
+    account: {
+      restaurants: { find },
+    },
+  },
+} = data;
+*/
