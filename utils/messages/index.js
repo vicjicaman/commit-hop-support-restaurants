58cefb43330ec74303a2bf90666f7c28247202f1
Victor Jimenez <vic.jicama@gmail.com>
2022-03-22 19:51:13 -0600

    Add utility to generate lang json files from csv

---
+++ utils/messages/index.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+const data = fs
+  .readFileSync("./support-restaurants - messages.csv")
+  .toLocaleString();
+
+const lines = data.split("\n");
+
+const res = {};
+const index = {};
+
+for (const idx in lines) {
+  const line = lines[idx];
+  const columns = line.trim().split(",");
+
+  for (const icx in columns) {
+    const value = columns[icx];
+
+    if (idx === "0") {
+      if (icx !== "0") {
+        res[value] = {};
+        index[icx] = value;
+      }
+    } else {
+      if (icx !== "0") {
+        res[index[icx]] = { ...res[index[icx]], [columns["0"]]: value };
+      }
+    }
+  }
+}
+
+console.log(JSON.stringify(res, null, 2));
+
+for (const code in res) {
+  fs.writeFileSync(`./dist/${code}.json`, JSON.stringify(res[code], null, 2));
+}
