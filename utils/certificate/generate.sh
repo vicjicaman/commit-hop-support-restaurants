faf891abd86377cd23df5bde4dcfc0beef791a4a
Victor Jimenez <vic.jicama@gmail.com>
2022-04-07 09:36:06 -0500

    Fix backend-static docker image Temporal hardcode path for non user owner resources in local dev

--- utils/certificate/generate.sh
+++ utils/certificate/generate.sh
@@ -1,5 +1,5 @@
 ENVFILE=$1
 
 docker pull certbot/dns-route53
-docker run -it --rm --env-file ${ENVFILE} -v ${PWD}/output:/etc/letsencrypt certbot/dns-route53 certonly -n --agree-tos --dns-route53 --email vic.jicama@gmail.com -d *.ua-wck.com
+docker run -it --rm --env-file ${ENVFILE} -v /home/victor/commit/user-data/certificates/output:/etc/letsencrypt certbot/dns-route53 certonly -n --agree-tos --dns-route53 --email vic.jicama@gmail.com -d *.ua-wck.com
 
