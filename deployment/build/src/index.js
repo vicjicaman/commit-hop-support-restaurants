be6f9ad229f3628231d3c8fc808721ab08925d79
Victor Jimenez <vic.jicama@gmail.com>
2022-04-14 10:07:26 -0500

    Add build target path variable

--- deployment/build/src/index.js
+++ deployment/build/src/index.js
@@ -14,6 +14,7 @@ const ComposeDataStep = require("./steps/compose-data")
 
 const SCOPE_NAME = process.env.SCOPE_NAME;
 const SCOPE_VERSION = process.env.SCOPE_VERSION;
+const BUILD_TARGET_PATH = process.env.BUILD_TARGET_PATH;
 
 const rootPath = "../..";
 const commonPath = path.join(rootPath, "common")
@@ -21,7 +22,7 @@ const componentsPath = path.join(commonPath, "components")
 const libsPath = path.join(rootPath, "libs");
 const scope = SCOPE_NAME;
 const version = SCOPE_VERSION;
-const outputPath = `/media/victor/helper/build/${scope}/${version}`;
+const outputPath = `${BUILD_TARGET_PATH}/${scope}/${version}`;
 
 
 (async () => {
