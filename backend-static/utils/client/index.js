952b28df155c531527d5549dfa8a36d0f05a206a
Victor Jimenez <vic.jicama@gmail.com>
2022-05-04 11:19:06 -0500

    Review inner server env

--- backend-static/utils/client/index.js
+++ backend-static/utils/client/index.js
@@ -2,6 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const SERVER_GRAPHQL = process.env["SERVER_GRAPHQL"];
 
+console.log(JSON.stringify(process.env));
 console.log(SERVER_GRAPHQL)
 const client = new ApolloClient({
   uri: `${SERVER_GRAPHQL}`,
