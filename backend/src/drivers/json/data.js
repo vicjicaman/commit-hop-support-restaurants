d8f743906478dd9c2424956541fa898535118546
Victor Jimenez <vic.jicama@gmail.com>
2022-05-10 15:08:27 -0500

    Add border points data

--- backend/src/drivers/json/data.js
+++ backend/src/drivers/json/data.js
@@ -127,7 +127,7 @@ export default {
             "country": "sk",
             "address": "",
             "images": ["https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Border_Uzhhorod-Vy%C5%A1n%C3%A9_Nemeck%C3%A9_-_Slovak_side-114045.jpg/800px-Border_Uzhhorod-Vy%C5%A1n%C3%A9_Nemeck%C3%A9_-_Slovak_side-114045.jpg"],
-            "latitude": 48.661111, 
+            "latitude": 48.661111,
             "longitude": 22.263611,
             "contact": "",
             "description": ""
@@ -138,11 +138,222 @@ export default {
             "country": "sk",
             "address": "",
             "images": ["https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Kostol%2C_ubla.JPG/450px-Kostol%2C_ubla.JPG"],
-            "latitude": 48.883333, 
-            "longitude": 22.383333, 
+            "latitude": 48.883333,
+            "longitude": 22.383333,
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "barabas-hu",
+            "name": "Barabás",
+            "country": "hu",
+            "address": "",
+            "images": ["https://www.ua-wck.com/flags/hu.png"],
+            "latitude": 48.23,
+            "longitude": 22.43,
+            "contact": "",
+            "description": ""
+        },
+
+        {
+            "id": "beregsurany-hu",
+            "name": "Beregsurány",
+            "country": "hu",
+            "address": "",
+            "images": ["https://www.ua-wck.com/flags/hu.png"],
+            "latitude": 48.166667,
+            "longitude": 22.55,
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "chop-railway-station-hu",
+            "name": "Chop railway station",
+            "country": "hu",
+            "address": "",
+            "images": ["https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/%D0%A7%D0%BE%D0%BF%2C_%D0%A3%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0.jpg/311px-%D0%A7%D0%BE%D0%BF%2C_%D0%A3%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0.jpg"],
+            "latitude": 48.432806,
+            "longitude": 22.207417,
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "eperjeske-hu",
+            "name": "Eperjeske",
+            "country": "hu",
+            "address": "",
+            "images": ["https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Eperjeske_rendez%C5%91_teherp%C3%A1lyaudvar%2C_1977_-_Fortepan_15340.jpg/706px-Eperjeske_rendez%C5%91_teherp%C3%A1lyaudvar%2C_1977_-_Fortepan_15340.jpg"],
+            "latitude": 48.35,
+            "longitude": 22.216667,
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "lonya-hu",
+            "name": "Lónya",
+            "country": "hu",
+            "address": "",
+            "images": ["https://upload.wikimedia.org/wikipedia/commons/d/d5/Nagyl%C3%B3nya_l%C3%A9gifot%C3%B3.jpg"],
+            "latitude": 48.323169,
+            "longitude": 22.274568,
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "luzhanka-hu",
+            "name": "Luzhanka",
+            "country": "hu",
+            "address": "",
+            "images": ["https://upload.wikimedia.org/wikipedia/commons/d/d5/Nagyl%C3%B3nya_l%C3%A9gifot%C3%B3.jpg"],
+            "latitude": 48.168889,
+            "longitude": 22.5975,
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "chop–tysa-hu",
+            "name": "Chop–Tysa",
+            "country": "hu",
+            "address": "",
+            "images": ["https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Tysa_bridge_Zahony-Chop.jpg/800px-Tysa_bridge_Zahony-Chop.jpg"],
+            "latitude": 48.418431,
+            "longitude": 22.170222,
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "solovka-railway-station-hu",
+            "name": "Solovka railway station",
+            "country": "hu",
+            "address": "",
+            "images": ["https://www.ua-wck.com/flags/hu.png"],
+            "latitude": 48.381192,
+            "longitude": 22.278748,
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "tiszabecs-hu",
+            "name": "Tiszabecs",
+            "country": "hu",
+            "address": "",
+            "images": ["https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Tisza%C3%BAjlak_-_Tiszabecs_nemzetk%C3%B6zi_hat%C3%A1r%C3%A1tkel%C5%91hely%2C_2011_Tisza%C3%BAjlak.jpg/800px-Tisza%C3%BAjlak_-_Tiszabecs_nemzetk%C3%B6zi_hat%C3%A1r%C3%A1tkel%C5%91hely%2C_2011_Tisza%C3%BAjlak.jpg"],
+            "latitude": 48.098719,
+            "longitude": 22.822231,
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "zahony-hu",
+            "name": "Záhony",
+            "country": "hu",
+            "address": "",
+            "images": ["https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Z%C3%A1hony_-_mad%C3%A1rt%C3%A1vlatb%C3%B3l.jpg/800px-Z%C3%A1hony_-_mad%C3%A1rt%C3%A1vlatb%C3%B3l.jpg"],
+            "latitude": 48.416667,
+            "longitude": 22.183333,
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "campulung-la-tisa-ro",
+            "name": "Câmpulung la Tisa",
+            "country": "ro",
+            "address": "",
+            "images": ["https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Campulung_la_Tisa_o.JPG/800px-Campulung_la_Tisa_o.JPG"],
+            "latitude": 47.985, 
+            "longitude": 23.7658,
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "chilia-veche-ro",
+            "name": "Chilia Veche",
+            "country": "ro",
+            "address": "",
+            "images": ["https://www.ua-wck.com/flags/ro.png"],
+            "latitude": 45.416667, 
+            "longitude": 29.283333, 
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "halmeu-ro",
+            "name": "Halmeu",
+            "country": "ro",
+            "address": "",
+            "images": ["https://www.ua-wck.com/flags/ro.png"],
+            "latitude": 47.966667, 
+            "longitude": 23.016667, 
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "kiliia-ro",
+            "name": "Kiliia",
+            "country": "ro",
+            "address": "",
+            "images": ["https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/%D0%A1%D1%82%D0%B0%D1%80%D0%BE%D0%B2%D0%B8%D0%BD%D0%BD%D0%B0_%D0%B2%D1%83%D0%BB%D0%B8%D1%86%D1%8F.jpg/800px-%D0%A1%D1%82%D0%B0%D1%80%D0%BE%D0%B2%D0%B8%D0%BD%D0%BD%D0%B0_%D0%B2%D1%83%D0%BB%D0%B8%D1%86%D1%8F.jpg"],
+            "latitude": 45.45, 
+            "longitude": 29.266667, 
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "musenita-ro",
+            "name": "Mușenița",
+            "country": "ro",
+            "address": "",
+            "images": ["https://upload.wikimedia.org/wikipedia/commons/a/ae/Bainet1.jpg"],
+            "latitude": 47.966667, 
+            "longitude": 26, 
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "sighetu-marmatiei-ro",
+            "name": "Sighetu Marmației",
+            "country": "ro",
+            "address": "",
+            "images": ["https://upload.wikimedia.org/wikipedia/commons/4/4b/Zentrum_Sighet_3.jpg"],
+            "latitude": 47.928611, 
+            "longitude": 23.8925, 
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "siret-ro",
+            "name": "Siret",
+            "country": "ro",
+            "address": "",
+            "images": ["https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Fosta_prim%C4%83rie_a_ora%C8%99ului_Siret.JPG/800px-Fosta_prim%C4%83rie_a_ora%C8%99ului_Siret.JPG"],
+            "latitude": 47.953056, 
+            "longitude": 26.0725,  
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "tulcea-ro",
+            "name": "Tulcea",
+            "country": "ro",
+            "address": "",
+            "images": ["https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Administra%C8%9Bia_Pesc%C4%83riei_Statului.jpg/800px-Administra%C8%9Bia_Pesc%C4%83riei_Statului.jpg"],
+            "latitude": 45.19,  
+            "longitude": 28.8,  
+            "contact": "",
+            "description": ""
+        },
+        {
+            "id": "vylkove-ro",
+            "name": "Vylkove",
+            "country": "ro",
+            "address": "",
+            "images": ["https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/%D0%92%D0%B8%D0%BB%D0%BA%D0%BE%D0%B2%D0%B5.jpg/250px-%D0%92%D0%B8%D0%BB%D0%BA%D0%BE%D0%B2%D0%B5.jpg"],
+            "latitude": 45.399167, 
+            "longitude": 29.593611,  
             "contact": "",
             "description": ""
         }
+
     ],
     "restaurants": [
         {
